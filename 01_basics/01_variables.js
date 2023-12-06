3f99aacbfe9f27e455db3200ccb260f569dedb59
GauriAdekar <118828554+GauriAdekar@users.noreply.github.com>
2023-12-06 06:47:51 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId= 12345
+let accountEmail= "gauri@gmail.com"
+var accountPassword = "1234"
+accountCity = "Pune" 
+//  accountId = 23                 // not allowed
+accountEmail = "adekar@gmail.com"
+accountPassword = "345"
+accountCity = "Indapur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
