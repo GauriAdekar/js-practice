adaf02ba1b3596dbc8d53dc1e68e8a527f1ac2dd
GauriAdekar <118828554+GauriAdekar@users.noreply.github.com>
2023-12-08 09:09:17 +0000

    String Functions

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,23 @@
+const name = "gauriadekar"
+const value = 500
+//console.log(`Hello my name is ${name} and my marks are ${value}`);       // String interpolation
+
+ const gameName = new String('gauri-ga')
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('r'));
+// console.log(gameName.substring(0,3));
+// console.log(gameName.slice(1,3));
+
+const myName = "Adekar_gauri_rajendra"
+// console.log(myName);
+// console.log(myName.trim());
+
+// const url = "https://gauri.com/adekar%20rajendra"
+// console.log(url.replace('%20','_'));
+// console.log(url.includes('gauri'));
+console.log(myName.split('_'));
