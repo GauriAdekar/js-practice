fcaba63f2dfaf5d9ec90e29f5e49d3953fc83094
GauriAdekar <118828554+GauriAdekar@users.noreply.github.com>
2023-12-17 10:06:17 +0000

    Arrays in JS

---
+++ 02_basics/01_array.js
@@ -0,0 +1,21 @@
+const myArray = [0, 1, 2, 3, 4, 5]               // 1st way to declare array
+//console.log(myArray);
+const myHeros = ["shaktiman", "naagraj"]
+//console.log(myHeros[1]);
+
+// const myArray2 = new Array(1,2,3,4,)          // 2nd way to declare array
+// console.log(myArray2[3]);
+// myHeros.push("mama")
+// console.log(myHeros)
+// myHeros.pop()
+// console.log(myHeros);
+
+// myArray.unshift(5)
+// myArray.shift()
+// console.log(myArray.includes(8));
+// console.log(myArray.indexOf(8));
+
+// const newArr = myArray.join()            // changes the type to string 
+// console.log(myArray);
+// console.log(newArr);
+
