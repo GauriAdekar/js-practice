5affcb941a8dc2a78516059263a27ca62a4d1f70
GauriAdekar <118828554+GauriAdekar@users.noreply.github.com>
2023-12-28 10:21:25 +0000

    hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,8 +6,49 @@ if(true)
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: ", a);
+    //console.log("INNER: ", a);
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+/********************* NESTED SCOPE **************/
+
+function one ()
+{
+    const username = "Gauri"
+
+    function two()
+    {
+        const website = "youtube"
+       // console.log(username);
+    }
+    console.log(website);
+    two()
+}
+//one()
+
+if(true)
+{
+    const username = "Gauri"
+    if(username === "Gauri")
+    {
+        const website = " youtube"
+       // console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+addOne(5)
+function addOne(num)
+{
+    return num + 1
+}
+// this will not give error becoz it is only declared
+
+addTwo(4)
+const addTwo = function(num){                             // sometimes called as expression
+    return num + 2                                        // HOISTING
+}
+// this gives error becoz it is initialized
