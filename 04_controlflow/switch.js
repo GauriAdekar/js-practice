6219baa3c1dc9870c5b83d8125863ddcdc99028c
GauriAdekar <118828554+GauriAdekar@users.noreply.github.com>
2023-12-30 10:02:59 +0000

    switch in js

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,31 @@
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//             console.log("february");
+//             break;
+//      case 3:
+//          console.log("march");
+//             break;
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+            console.log("february");
+            break;
+     case "march":
+         console.log("march");
+            break;
+    default:
+        console.log("default case match");
+        break;
+}
